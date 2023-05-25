3e9e3c3c006c7096ebb8b7d8f781d96ad461a7bc
Weerawut Chaiyasomboon <weerawut@live.com>
2023-05-25 21:06:04 +0700

    add services for prisma

---
+++ src/services/newstype.service.ts
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import { NewsType, Prisma } from "@prisma/client";
+
+export async function findAllNewsType(): Promise<NewsType[]> {
+    return await prisma.newsType.findMany({
+        orderBy: { id: "desc"}
+    });
+}
+
+export async function findOneNewsType(id: number): Promise<NewsType | null> {
+    return await prisma.newsType.findUnique({
+        where: { id: id },
+    });
+}
+
+export async function createNewsType(data: Prisma.NewsTypeCreateInput): Promise<NewsType> {
+    return await prisma.newsType.create({
+        data: {title: data.title }
+    });
+}
+
+export async function updateNewsType(id: number, data: Prisma.NewsTypeUpdateInput): Promise<NewsType> {
+    return await prisma.newsType.update({
+        where: { id: id},
+        data: data,
+    });
+}
+
+export async function removeNewsType(id: number): Promise<NewsType> {
+    return await prisma.newsType.delete({
+        where: { id: id },
+    });
+}
